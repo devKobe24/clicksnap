47dce015a8bb329f507918840cbd4be979f2f51a
devKobe24 <dev.skyachieve91@gmail.com>
2026-01-08 03:54:52 +0900

    first commit

---
+++ src/main/java/com/clicksnap/ClicksnapApplication.java
@@ -0,0 +1,13 @@
+package com.clicksnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicksnapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClicksnapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/clicksnap/ClicksnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clicksnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicksnapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
